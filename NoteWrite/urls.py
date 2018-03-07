f4432e50b5f76c1f944c0a47e2bb6f395d5ad905
vaibhavkollipara <vaibhav.kollipara@gmail.com>
2018-03-07 14:47:45 -0600

    Added jwt auth.

--- NoteWrite/urls.py
+++ NoteWrite/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import (obtain_jwt_token,
+                                      verify_jwt_token,
+                                      refresh_jwt_token
+                                      )
 
 urlpatterns = [
     path('', include('app.urls')),
+    path('auth/obtaintoken', obtain_jwt_token),
+    path('auth/verifytoken', verify_jwt_token),
+    path('auth/refreshtoken', refresh_jwt_token),
     path('admin/', admin.site.urls),
 ]
